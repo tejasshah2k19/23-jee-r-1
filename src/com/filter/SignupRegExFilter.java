34f1e4115af5c65abbda1d1d230a9883bdcd3a13
tejas <tejasshah@gmail.com>
2023-07-10 19:59:38 +0530

    multi filter

--- src/com/filter/SignupRegExFilter.java
+++ src/com/filter/SignupRegExFilter.java
@@ -10,11 +10,12 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
-import org.omg.CORBA.Request;
+import com.util.Validators;
 
-public class SignupFilter implements Filter {
+public class SignupRegExFilter implements Filter {
 
 	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
 
 	}
 
@@ -26,24 +27,23 @@ public class SignupFilter implements Filter {
 		// password
 
 		boolean isError = false;
-		if (firstName == null || firstName.trim().length() == 0) {
+		if (Validators.isAlpha(firstName) == false) {
 			isError = true;
-			request.setAttribute("firstNameError", "Please Enter FirstName");
+			request.setAttribute("firstNameError", "Please Enter Valid FirstName [a-zA-Z] ");
 		}
 
 		if (isError == true) {
-			//goBack
+			// goBack
 			RequestDispatcher rd = request.getRequestDispatcher("FilterSignup.jsp");
 			rd.forward(request, response);
 		} else {
-			//rd 
-			chain.doFilter(request, response);// goahead --> servlet | filter 
+			// rd
+			chain.doFilter(request, response);// goahead --> servlet | filter
 		}
-
 	}
 
 	public void destroy() {
+		// TODO Auto-generated method stub
 
 	}
-
 }
